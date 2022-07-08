6285d5b0c087c3264c5bdcfdfabbda750bbf3519
muroon <toyohito.muro@gmail.com>
2022-07-08 20:13:10 +0900

    divide the package into two

---
+++ poollog/event_test.go
@@ -0,0 +1,44 @@
+package poollog
+
+import "testing"
+
+func TestNewEvent(t *testing.T) {
+	stream := &testStream{}
+	LogWriter = stream
+	Info("test")
+	t.Log(stream.writeCount)
+}
+
+func BenchmarkEvent(b *testing.B) {
+	stream := &testStream{}
+	LogWriter = stream
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			Info("test")
+		}
+	})
+
+	if stream.WriteCount() != uint64(b.N) {
+		b.Fatalf("Log write count")
+	}
+}
+
+func BenchmarkEventWithDone(b *testing.B) {
+	stream := &testStream{}
+	LogWriter = stream
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			InfoWithDone("test", doneFunc)
+		}
+	})
+
+	if stream.WriteCount() != uint64(b.N) {
+		b.Fatalf("Log write count")
+	}
+}
